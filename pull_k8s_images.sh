5ff24d2095b1a542bf80cdfcf76f5a6c2165bcb2
jancco <jancco2012@gmail.com>
2018-09-27 23:40:33 +0800

    Add k8s.gcr.io/etcd-amd64:3.2.18

--- pull_k8s_images.sh
+++ pull_k8s_images.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-
 trans_image_names=( \
     gcr.io_google_containers_defaultbackend
 	gcr.io_google_containers_metrics-server-amd64
 	k8s.gcr.io_coredns
 	k8s.gcr.io_elasticsearch
+	k8s.gcr.io_etcd-amd64
 	k8s.gcr.io_fluentd-elasticsearch
 	k8s.gcr.io_kube-apiserver-amd64
 	k8s.gcr.io_kube-controller-manager-amd64
